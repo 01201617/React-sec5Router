063945c08af27201befb1cd450180b9a5b45cdc2
Nan <40654091+01201617@users.noreply.github.com>
2021-11-11 16:00:18 +0900

    ネストページ遷移

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Pase2";
 import "./styles.css";
 
@@ -19,9 +21,22 @@ export default function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/page1">
-          <Page1 />
-        </Route>
+        <Route
+          path="/page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              <Route exact path={url}>
+                <Page1 />
+              </Route>
+              <Route path="/page1/detailA">
+                <Page1DetailA />
+              </Route>
+              <Route path="/page1/detailB">
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        ></Route>
         <Route path="/page2">
           <Page2 />
         </Route>
